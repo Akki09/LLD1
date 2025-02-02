d8e4f86736b6ff7a9c1bb085ba6f5c1008f4ad9c
Akshay Vakharia <Akshayvakharia1995@gmail.com>
2025-02-02 22:13:00 +0530

    Access Modifier

--- src/main/java/com/learning/accessmodifier/diffpackage/Employee.java
+++ src/main/java/com/learning/accessmodifier/diffpackage/Employee.java
@@ -1,4 +1,25 @@
 package com.learning.accessmodifier.diffpackage;
 
+import com.learning.accessmodifier.Student;
+
 public class Employee {
+
+    String employeeType;
+
+    public String getEmployeeType() {
+        return employeeType;
+    }
+
+    public void setEmployeeType(String employeeType) {
+        this.employeeType = employeeType;
+    }
+
+    public void Test() {
+        Student s = new Student();
+        s.name = "Akshay";
+        // Error: java: percentage has protected access in com.learning.accessmodifier.Student
+        // As this employee class does not extend Student class so we can't access the protected
+        // data members in different package
+        //s.percentage = 98.0;
+    }
 }
